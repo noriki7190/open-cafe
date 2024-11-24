265bccf15215cce85b217d0149c101f4d0326395
noriki7190 <noriki7190@gmail.com>
2024-11-24 22:54:01 +0900

    ハンバーガーメニュー完成

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 $(function () {
+  // swipre
   const swiper = new Swiper(".swiper", {
     // loop: true,
     // effect: 'fade',
@@ -7,5 +8,28 @@ $(function () {
       el: ".swiper-pagination",
       clickable: true,
     },
+  });
+
+  // ハンバーガーメニュー
+  const header = $("#js-header");
+  const hamburger = $("#js-hamburger");
+  $('#js-hamburger').on('click', function () {
+    $('body').toggleClass('is-open');
+  });
+
+
+  // トップへ戻る
+  const pagetop = $('#js-page-top');
+  pagetop.hide();
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      pagetop.fadeIn();
+    } else {
+      pagetop.fadeOut();
+    }
+  });
+  pagetop.click(function () {
+    $('body, html').animate({ scrollTop: 0 }, 500);
+    return false;
   });
 });
